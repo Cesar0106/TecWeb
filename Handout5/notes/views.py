7fb64bc9337528a41284819b02a9148dbafcf27d
Cesar0106 <62568082+Cesar0106@users.noreply.github.com>
2021-09-14 08:46:11 -0300

    Tudo pronto

--- Handout5/notes/views.py
+++ Handout5/notes/views.py
@@ -7,6 +7,11 @@ def index(request):
         title = request.POST.get('titulo')
         content = request.POST.get('detalhes')
         # TAREFA: Utilize o title e content para criar um novo Note no banco de dados
+        #Note.objects.create(title = title, content= content)(Outro método para salvar os dados)
+        note = Note()
+        note.title = title
+        note.content = content
+        note.save()
         return redirect('index')
     else:
         all_notes = Note.objects.all()

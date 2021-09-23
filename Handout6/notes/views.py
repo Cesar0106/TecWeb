a539f36e9d05307d9c3e817cd381174a1b210331
Cesar0106 <cesarea@al.insper.edu.br>
2021-09-23 15:26:56 -0300

    Handout6 feito

---
+++ Handout6/notes/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from .models import Note
+
+
+def index(request):
+    if request.method == 'POST':
+        title = request.POST.get('titulo')
+        content = request.POST.get('detalhes')
+        # TAREFA: Utilize o title e content para criar um novo Note no banco de dados
+        #Note.objects.create(title = title, content= content)(Outro método para salvar os dados)
+        note = Note()
+        note.title = title
+        note.content = content
+        note.save()
+        return redirect('index')
+    else:
+        all_notes = Note.objects.all()
+        return render(request, 'notes/index.html', {'notes': all_notes})
